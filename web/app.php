15cd4ce2a976e1dc719caad125b2e85e9e6c71f8
Andrei Lobach <andrei.lobach@outlook.com>
2019-03-07 14:04:39 +0300

    add server logs

--- web/app.php
+++ web/app.php
@@ -6,7 +6,6 @@ use Service\HTTPExceptionsRenderer;
 
 require_once __DIR__.'/../app/autoload.php';
 $kernel = new Kernel;
-
 try {
     $request = RequestFactory::createRequest();
     $response = $kernel->createResponse($request);
@@ -15,10 +14,8 @@ try {
    $response = $kernel->getContainer()->get(HTTPExceptionsRenderer::class)->createResponse($exception);
    $response->send();
 } catch (Exception $exception) {
-    echo $exception->getCode();
-    echo $exception->getMessage();
+    file_put_contents(__DIR__ . '/../logs/server_log.log', [$exception->getTraceAsString(), $exception->getMessage()], FILE_APPEND | LOCK_EX);
 }
-
 
 
 
